8db5caeec3c556104439df0176b062a8fa86b5d7
Jacqueline Chávez <jacquelinechavez301@gmail.com>
2023-04-03 08:51:15 -0600

    Edición en tabla cliente y usuario

--- BD/BD-18-03-2023/PROYECTO LIS.sql
+++ BD/BD-18-03-2023/PROYECTO LIS.sql
@@ -60,8 +60,9 @@ CREATE TABLE Cliente (
     Telefono VARCHAR(8) NOT NULL,
     Direccion VARCHAR (255) NOT NULL,
     Token VARCHAR(10) NOT NULL,
-	Fecha_Verificacion_Email DATETIME NOT NULL,
-    PRIMARY KEY (DUI)
+    ID_Usuario INT NOT NULL,
+    PRIMARY KEY (DUI),
+    KEY `fk_empleado_usuario` (`ID_Usuario`)
 )ENGINE=InnoDB;
 CREATE TABLE Empleado (
 	ID_Empleado INT NOT NULL,
@@ -87,41 +88,41 @@ CREATE TABLE Cupon (
     KEY `fk_cupon_oferta`(`ID_Oferta`),
     KEY `fk_cupon_estado_cupon` (`ID_Estado_Cupon`)
 )ENGINE=InnoDB;
-INSERT INTO estado_oferta (ID_EstadoOferta, Nombre) VALUES ('1', 'Espera de aprobación'), ('2', 'Aprobadas futuras '),
+INSERT INTO estado_oferta (ID_EstadoOferta, Nombre) VALUES ('1', 'Espera de aprobaciï¿½n'), ('2', 'Aprobadas futuras '),
  ('3', 'Activas'), ('4', ' Pasadas'),
  ('5', 'Rechazadas'), ('6', 'Descartadas');
 INSERT INTO rubro (ID_Rubro, Nombre) 
-VALUES ('1', 'Salón de belleza'), ('2', 'Restaurante '),
-('3', 'Taller'), ('4', 'Estructuras metálicas'),
+VALUES ('1', 'Salï¿½n de belleza'), ('2', 'Restaurante '),
+('3', 'Taller'), ('4', 'Estructuras metï¿½licas'),
  ('5', 'Super mercado'), ('6', 'Farmacia');
 INSERT INTO empresa (ID_Empresa, Nombre, Direccion, NombreContacto, Telefono, Correo, PorcentajeComision, ID_Rubro) 
 VALUES 
-('EMP001', 'Pollo de Oro', 'Blvr. del ejército, Centro comercial Plaza Mundo, 2° nivel, Soyapango ', 'Mario Rivas', '77223344', 'armando.lopez@lis.com', '0.1', '2'), ('EMP002', 'El Super De Todos', 'PR2X+6P5, Blvr. del Ejercito Nacional, Soyapango', 'Luis Garcia', '77669955', 'super.todos@lis.com', '0.05', '5'),
- ('EMP003', 'Farmacia Amigo', 'Frente a, Entre 5a y 7a Avenida Norte y, 15 Calle Pte., San Salvador', 'Arturo Ramírez ', '66789045', 'farma.amigo@gmail.com', '0.15', '6'), ('EMP004', 'Destellos Vicky Salón ', 'P.º Gral. Escalón 3656, San Salvador', 'Victoria Flores', '77452987', 'destellos.vicky@lis.com', '0.08', '1');
+('EMP001', 'Pollo de Oro', 'Blvr. del ejï¿½rcito, Centro comercial Plaza Mundo, 2ï¿½ nivel, Soyapango ', 'Mario Rivas', '77223344', 'armando.lopez@lis.com', '0.1', '2'), ('EMP002', 'El Super De Todos', 'PR2X+6P5, Blvr. del Ejercito Nacional, Soyapango', 'Luis Garcia', '77669955', 'super.todos@lis.com', '0.05', '5'),
+ ('EMP003', 'Farmacia Amigo', 'Frente a, Entre 5a y 7a Avenida Norte y, 15 Calle Pte., San Salvador', 'Arturo Ramï¿½rez ', '66789045', 'farma.amigo@gmail.com', '0.15', '6'), ('EMP004', 'Destellos Vicky Salï¿½n ', 'P.ï¿½ Gral. Escalï¿½n 3656, San Salvador', 'Victoria Flores', '77452987', 'destellos.vicky@lis.com', '0.08', '1');
 Insert INTO  oferta (ID_Oferta , Titulo, CantLimite, Descripcion,Detalles,FechaInicio, FechaFin, PrecioOriginal, PrecioOferta, Imagen,ID_Empresa, ID_EstadoOferta, Justificacion, FechaLimite)
 values ('1', 'Domingos de Familia', '80', 'La familia solo pagara la mitad de cada Plato del restaurante', 'Incluye postres y bebida ', '2023-05-01','2023-08-01','9.50','4.75','https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9781/4842/9781484217290.jpg','EMP001','3', '', '2023-08-01'),
 ('2', 'Lunes de hamburguesas al 2X1 ', '80', 'Por la compra de una hamburguesa te llevas dos ', 'Incluye papas y bebida ', '2023-05-01', '2023-08-01','14','7','https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9781/4842/9781484217290.jpg','EMP001','3', '', '2023-08-01'),
 ('3', 'Miercoles de alitas', '80', 'Al compar el primer combo de alitas tellevas el Segundo a mitad de precio', 'Incluye papas y bebida', '2023-05-01', '2023-08-01','16','12','https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9781/4842/9781484217290.jpg','EMP001','1', '', '2023-08-01'), 
 ('4', 'Viernes de pizza ', '80', 'Todas las pizza amitad de precio', 'Incluye Bebida', '2023-05-01', '2023-08-01','9.50','4.75','https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9781/4842/9781484217290.jpg','EMP001','2', '', '2023-08-01'),
 ('5', 'Martes de cortes', '100', 'por el primer corte de cabello el segundo amitad de precio', 'Cualquier estilo de corte', '2023-05-01', '2023-08-01','6','4.50','https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9781/4842/9781484217290.jpg','EMP004','3', '', '2023-08-01') ,
-('6', 'Manicura al 2x1', '100', 'por la primera manicura te llevas la segunda gratis ', 'No importa el largo de la uña', '2023-05-01', '2023-08-01','24','12','https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9781/4842/9781484217290.jpg','EMP004','3', '', '2023-08-01'),
+('6', 'Manicura al 2x1', '100', 'por la primera manicura te llevas la segunda gratis ', 'No importa el largo de la uï¿½a', '2023-05-01', '2023-08-01','24','12','https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9781/4842/9781484217290.jpg','EMP004','3', '', '2023-08-01'),
 ('7', 'Miercoles de alisados', '100', 'Todos los alisados con un 40% de descuento', 'No importa el largo de cabello y el estilo de alisado', '2023-05-01', '2023-08-01','25','15','https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9781/4842/9781484217290.jpg','EMP004','3', '', '2023-08-01'),
 ('8', 'Tinte mas corte de cabello ', '100', 'por la compra de un tinte te llevas un corte de cabello gratis' , 'Cualquier largo de cabello', '2023-05-01', '2023-08-01','6','4.50','https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9781/4842/9781484217290.jpg','EMP004','1', '', '2023-08-01'),
 ('9', 'Pedicura y manicura', '100', 'por el precio de uno te llevas el otro gratis', 'Cualquier dia de la semana', '2023-05-01', '2023-08-01','20','10','https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9781/4842/9781484217290.jpg','EMP004','3', '', '2023-05-01'),
 ('10', 'Corte de cabello mas mechas', '100', 'Por un corte de cabello puedes pedir tus mechas gratis', 'Cualquier estilo de corte', '2023-05-01', '2023-08-01','6','4.50','https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9781/4842/9781484217290.jpg','EMP004','3', '', '2023-08-01'),
-('11', 'Jueves dia de Pigmentación con hena ', '100', 'Pigmenta tus cejas amitad de precio', 'Cualquier tono de hena', '2023-05-01', '2023-08-01','6','4.50','https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9781/4842/9781484217290.jpg','EMP004','4', '', '2023-08-01'),
-('12', 'Jamón al 2x1', '100', 'por la compra de un jamón te llevas el segundo gratis', 'valido todos los fines de semana', '2023-05-01', '2023-05-01','6','4.50','https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9781/4842/9781484217290.jpg','EMP001','6', 'No cumple los reglametos de la cuponeria', '2023-05-01'),
+('11', 'Jueves dia de Pigmentaciï¿½n con hena ', '100', 'Pigmenta tus cejas amitad de precio', 'Cualquier tono de hena', '2023-05-01', '2023-08-01','6','4.50','https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9781/4842/9781484217290.jpg','EMP004','4', '', '2023-08-01'),
+('12', 'Jamï¿½n al 2x1', '100', 'por la compra de un jamï¿½n te llevas el segundo gratis', 'valido todos los fines de semana', '2023-05-01', '2023-05-01','6','4.50','https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9781/4842/9781484217290.jpg','EMP001','6', 'No cumple los reglametos de la cuponeria', '2023-05-01'),
 ('13', 'Viertes todo amitad de precio', '100', 'Todos los productos a mitad de precio', 'No importa la cantidad de productos', '2023-05-01', '2023-08-01','6','4.50','https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9781/4842/9781484217290.jpg','EMP002','3', '', '2023-08-01'),
 ('14', 'Dia de frutas', '100', 'La libra de frutas con un 40% de descuento', 'Cualquier estilo de corte', '2023-05-01', '2023-08-01','4','2.40','https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9781/4842/9781484217290.jpg','EMP002','3', '', '2023-08-01'),
 ('15', 'Sabado de mechas', '100', 'Mechas con el 25% de descuento', 'No importa el largo del cabello', '2023-04-01', '2023-04-03','25','15','https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9781/4842/9781484217290.jpg','EMP004','5', 'No cumple los reglametos de la cuponeria ', '2023-04-03');
 INSERT INTO usuario (ID_Usuario, Nombres, Apellidos, Contrasenia, Correo, Tipo) 
 VALUES 
- ('0' , 'Armando' , 'López', '234567', 'armando.lopez@lis.com', 'Administrador'),
- ('1', 'Romero ', 'Hernandez', '123456', 'romero.hernandez@lis.com', 'Cliente');
+ ('0' , 'Armando' , 'Lï¿½pez', '234567', 'armando.lopez@lis.com', 'Administrador'),
+ ('1', 'Richard Mario ', 'Molina Aguilar', '1599', 'semita@horchata.com', 'Cliente');
 INSERT INTO empleado(ID_Empleado, ID_Empresa, ID_Usuario, Tipo)
 VALUES ('1', 'EMP001', '1',  'Administrador');
-Insert Into cliente (DUI, Nombres,Apellidos,Contrasenia,Correo,Telefono , Direccion , Token,Fecha_Verificacion_Email) 
-VALUES  ('00167564', 'Richard Mario','Molina Aguilar','sddfsfdsdsfg','semita@horchata.com','75080845', 'Ciudad delgado, canton plan del pino colonia mercedes casa 30A ', 'cliente1','2023-03-17 11:53:02');
+Insert Into cliente (DUI, Nombres,Apellidos,Contrasenia,Correo,Telefono , Direccion , Token, ID_Usuario) 
+VALUES  ('00167564', 'Richard Mario','Molina Aguilar','1599','semita@horchata.com','75080845', 'Ciudad delgado, canton plan del pino colonia mercedes casa 30A ', 'cliente1', '1');
 INSERT INTO estado_Cupon( ID_Estado_Cupon, Estado)
 VALUES
 ('01', 'Canjeado'), ('02', 'Sin canjear');
