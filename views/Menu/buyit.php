d0a01940090a555bd58b7c3f4ea771b696b5a1f6
Jacqueline Chávez <jacquelinechavez301@gmail.com>
2023-04-04 16:53:05 -0600

    categoria belleza

--- views/Menu/buyit.php
+++ views/Menu/buyit.php
@@ -84,25 +84,7 @@
                       <p><?php echo $cupones['PrecioOferta'] ?></p>
                       <a href="index.php?c=Inicio&a=carrito&id=<?=$cupones['ID_Oferta']?>" class="btn btn-primary">Agregar al carrito nuevo</a>
                       </form>
-                        <!-- Modal -->
-                        <div class="modal fade" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                          <div class="modal-dialog">
-                            <div class="modal-content">
-                              <div class="modal-header">
-                                <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
-                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                              </div>
-                              <div class="modal-body">
-                                ...
-                              </div>
-                              <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                <button type="button" class="btn btn-primary">Save changes</button>
-                              </div>
-                            </div>
-                          </div>
-                        </div>  
-                        <!--End modal-->                
+                                    
                     </div>    
                   </div>
                 </div>
--- views/Menu/pages/Belleza.php
+++ views/Menu/pages/Belleza.php
@@ -23,15 +23,15 @@
          
                 <div class="col-md-4">
                   <div class="card">
-                    <div class="img1"><img src="<?php echo $cupones[0]['Imagen'] ?>" alt=""></div><!--Fondo CARD-->
+                    <div class="img1"><img src="<?php echo $cupones['Imagen'] ?>" alt=""></div><!--Fondo CARD-->
                     <div class="img2"><img src="img/ramen_3.jpg" alt=""></div>                  
                     <div class="main-text">
-                      <h1><?php echo $cupones[0]['Titulo'] ?></h1>
-                      <p>$<?php echo $cupones[0]['PrecioOferta'] ?></p>
+                      <h1><?php echo $cupones['Titulo'] ?></h1>
+                      <p>$<?php echo $cupones['PrecioOferta'] ?></p>
                       <form action="" method="post">
-                        <input type="hidden" name="id" id="id" value="<?php echo  $cupones[0]['ID_Oferta'];?>">
-                        <input type="hidden" name="nombre" id="nombre" value="<?php echo $cupones[0]['Titulo'];?>">
-                        <input type="hidden" name="precio" id="precio" value="<?php echo $cupones[0]['PrecioOferta'];?>">
+                        <input type="hidden" name="id" id="id" value="<?php echo  $cupones['ID_Oferta'];?>">
+                        <input type="hidden" name="nombre" id="nombre" value="<?php echo $cupones['Titulo'];?>">
+                        <input type="hidden" name="precio" id="precio" value="<?php echo $cupones['PrecioOferta'];?>">
                         <input type="hidden" name="cantidad" id="cantidad" value="<?php echo 1;?>">
                         <button class="btn btn-primary" 
                                 type="submit" 
