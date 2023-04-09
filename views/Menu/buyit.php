650aa2f08672c31260828656059161a23e6ca850
Ander503 <andersonmelendez73@gmail.com>
2023-04-08 21:52:51 -0600

    Arreglando headers y menu de cada pagina

--- views/Menu/buyit.php
+++ views/Menu/buyit.php
@@ -10,54 +10,66 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     <!--Propio-->
     <link rel="stylesheet" href="css/style_1.css">
+    <link rel="stylesheet" href="css/drop.css">
+    
     <!--Slider-->
     <link rel="stylesheet" href="css/style_2_s.css">
   </head>
-  <body>
-  <header class="bo_dy">
+
+<body>
+
+<header>
     <!--Ingresando el nuevo menu-->
     <nav class="navbar navbar-expand-lg  fixed-top" style="background-color: #86A3B8;">
       <div class="container-fluid">
-         <!--Logo-->
-        <a class="navbar-brand" href="#">
+        <!--Logo-->
+        <a class="navbar-brand" href="index.php?c=inicio">
           <img class="logotipo" src="img/Logo_sin_slogan_t.png" alt="">
         </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link" href="index.php?c=categoria">Categorias</a>
-            </li>
-           <!-- <li class="nav-item">
-              <a class="nav-link" href="index.php?c=usuario">prueba login</a>
-            </li>-->
-            <li class="nav-item">
-              <a class="nav-link" href="index.php?c=Inicio&a=mostrarCarrito"><i class="fa-solid fa-cart-shopping"></i> (<?php echo (empty($_SESSION['CARRITO'])?0:array_sum(array_column($_SESSION['CARRITO'],"CANTIDAD")));?>)</a>
-			  <!--Nota jacky le borre () y lo demas lo comente  -->
-            </li>
-          <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle"  role="button" data-bs-toggle="dropdown" aria-expanded="false">
-          <?php echo  isset($_SESSION['session'])?$_SESSION['session']['nombre']:"Login" ?> <i class="fa-solid fa-user"></a></i>
-          <ul class="dropdown-menu">
-            <?php if(!isset($_SESSION['session']))  { ?>
-            <li> <a class="dropdown-item " href="index.php?c=usuario"> Login</a></li>
-            <li><hr class="dropdown-divider"></li>
 
-            <?php  } else { ?>
-            
-            <li><a class="dropdown-item " href="index.php?c=cupon&a=ver_cupon">Historial de cupones</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li> <a class="dropdown-item " href="index.php?c=usuario&a=cambio">Cambiar Contraseña</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="index.php?c=usuario&a=logout">Cerrar Sesión</a></li>
-            <?php } ?>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+          
+          <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+
+              <li class="nav-item">
+                <a class="nav-link" href="index.php?c=categoria">Categorias</a>
+              </li>
+
+              <li class="nav-item">
+                 <a class="nav-link" href="index.php?c=Inicio&a=mostrarCarrito"><i class="fa-solid fa-cart-shopping"></i> (<?php echo (empty($_SESSION['CARRITO'])?0:array_sum(array_column($_SESSION['CARRITO'],"CANTIDAD")));?>)</a>			  
+              </li>
+              
+
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle"  role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  <?php echo  isset($_SESSION['session'])?$_SESSION['session']['nombre']:"Login" ?> <i class="fa-solid fa-user"></i>
+                </a>
+                <ul class="dropdown-menu">
+                  <?php if(!isset($_SESSION['session']))  { ?>
+
+                  <li> <a class="dropdown-item " href="index.php?c=usuario"> Login</a></li>
+                  <li><hr class="dropdown-divider"></li>
+
+                  <?php  } else { ?>                  
+
+                  <li><a class="dropdown-item " href="index.php?c=cupon&a=ver_cupon">Ver Cupones</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li> <a class="dropdown-item " href="index.php?c=usuario&a=cambio">Ajustes</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li><a class="dropdown-item" href="index.php?c=usuario&a=logout">Log out</a></li>
+
+                  <?php } ?>
+                </ul>
+              </li>
           </ul>
-          </ul>
-        </div>
       </div>
-    </nav>
+  </div>
+</nav>
+</header>
 	 <!--luego puede ir el resto de la pagina -->
     <!--Slider-->
     
@@ -79,6 +91,7 @@
     </div>
     <!--End Slider-->
     </header>
+    
     <Main>
     <!--BUIYT - CARDS -->
     <div class="profile-area">
@@ -111,8 +124,8 @@
         </div>
       </div>
     </div>
-    </Main>
-
+</Main>
+<!--End Main-->
    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
     <!--SLIDER-->
@@ -120,22 +133,17 @@
     <script src="js/slider.js"></script>
 
 
-  <footer>
-        <img class="logotipo-footer" src="img/Logo_sin_slogan_t.png" alt="logotipo">
-        <br>    
-        <p>
-        © Copyright  2023 BUYIT <br>
-          Pagina Creada por<br>
-          <i class="nav-link fa-brands fa-facebook "></i>
-        <i class="nav-link fa-brands fa-instagram"></i>
-        <i class="nav-link fa-brands fa-twitter"></i>
-          
-        </p>
-</footer>
-  </body>
-
-  <!--Pie de la pagina-->
- 
-
-
+    <footer>
+          <img class="logotipo-footer" src="img/Logo_sin_slogan_t.png" alt="logotipo">
+          <br>    
+          <p>
+          © Copyright  2023 BUYIT <br>
+            Pagina Creada por<br>
+            <i class="nav-link fa-brands fa-facebook "></i>
+          <i class="nav-link fa-brands fa-instagram"></i>
+          <i class="nav-link fa-brands fa-twitter"></i>
+            
+          </p>
+  </footer>
+</body>
 </html>
